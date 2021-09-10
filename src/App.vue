865c1361f861486e77668333e0656c7aa1c5f98a
VershkovAleksey <hagebang@gmail.com>
2021-09-10 14:31:55 +0300

    Update on vue3.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+        <DMU65></DMU65 >
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DMU65 from './components/DMU-65.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DMU65,
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/info.vue
@@ -0,0 +1,50 @@
+<template>
+
+<table id="info" width="400px" max-height="300px">
+<caption>DMU-65</caption>
+<tr>
+  <th>id</th>
+  <th>Название</th>
+  <th>Информация</th>
+</tr>
+<tr v-for="item in kontroledParams" :key="item.id">
+  <td>{{item.id}}</td>
+  <td>{{item.name}}</td>
+  <td>{{item.information}}</td>
+</tr>
+</table>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+  kontroledParams:[
+    {id:1,name: 'Неисправный элемент', information:'Подшипник 2 Х 466880' },
+    {id:2,name: 'Характеристики',information:'радиально-упорный шарикоподшипник, однорядный, для спаренного монтажа'},
+    {id:3,name: 'Производство',information:'SKF'},
+    {id:4,name: 'Аномальный контрольный параметр',information:'Виброскорость=6.5 м/с (в допуске <4.2м/с)' },
+    {id:5,name: 'Вид ремонта',information:'Замена'},
+    {id:6,name: 'Срочность ремонта',information:'неотложная' }
+  ]
+  }
+}
+}
+</script>
+
+<style>
+table{
+  max-height: 300px;
+}
+th{
+  border: 2px solid lightgray;
+}
+tr{
+border: 2px solid black;
+}
+td{
+  border: 1px solid gray;
+  align-items: center;
+  
+}
+</style>  
